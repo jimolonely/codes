4cbd49b4ac3efec5211f8d2508f0546c0af71ee8
jimolonely <wp147188@foxmail.com>
2018-07-18 17:51:17 +0800

    add LocalDateTime

--- project/README.md
+++ project/README.md
@@ -422,5 +422,36 @@ public void localDate() {
     System.out.println(date);
 }
 ```
+### 11.5 LocalDateTime
+```java
+@Test
+public void localDateTime() {
+    LocalDateTime sylvester = LocalDateTime.of(2014, Month.DECEMBER, 31, 23, 59, 59);
+
+    DayOfWeek dayOfWeek = sylvester.getDayOfWeek();
+    System.out.println(dayOfWeek);      // WEDNESDAY
+
+    Month month = sylvester.getMonth();
+    System.out.println(month);          // DECEMBER
+
+    long minuteOfDay = sylvester.getLong(ChronoField.MINUTE_OF_DAY);
+    System.out.println(minuteOfDay);    // 1439
+
+    Instant instant = sylvester
+            .atZone(ZoneId.systemDefault())
+            .toInstant();
+
+    Date legacyDate = Date.from(instant);
+    System.out.println(legacyDate);     // Wed Dec 31 23:59:59 CET 2014
+
+    DateTimeFormatter formatter =
+            DateTimeFormatter
+                    .ofPattern("MMM dd, yyyy - HH:mm");
+
+    LocalDateTime parsed = LocalDateTime.parse("Nov 03, 2014 - 07:13", formatter);
+    String string = formatter.format(parsed);
+    System.out.println(string);     // Nov 03, 2014 - 07:13
+}
+```
 
 
--- project/src/main/java/java8/test/DateTest.java
+++ project/src/main/java/java8/test/DateTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.Locale;
@@ -74,5 +75,34 @@ public class DateTest {
         DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(Locale.CHINA);
         LocalDate date = LocalDate.parse("1.10.2018", formatter);
         System.out.println(date);
+    }
+
+    @Test
+    public void localDateTime() {
+        LocalDateTime sylvester = LocalDateTime.of(2014, Month.DECEMBER, 31, 23, 59, 59);
+
+        DayOfWeek dayOfWeek = sylvester.getDayOfWeek();
+        System.out.println(dayOfWeek);      // WEDNESDAY
+
+        Month month = sylvester.getMonth();
+        System.out.println(month);          // DECEMBER
+
+        long minuteOfDay = sylvester.getLong(ChronoField.MINUTE_OF_DAY);
+        System.out.println(minuteOfDay);    // 1439
+
+        Instant instant = sylvester
+                .atZone(ZoneId.systemDefault())
+                .toInstant();
+
+        Date legacyDate = Date.from(instant);
+        System.out.println(legacyDate);     // Wed Dec 31 23:59:59 CET 2014
+
+        DateTimeFormatter formatter =
+                DateTimeFormatter
+                        .ofPattern("MMM dd, yyyy - HH:mm");
+
+        LocalDateTime parsed = LocalDateTime.parse("Nov 03, 2014 - 07:13", formatter);
+        String string = formatter.format(parsed);
+        System.out.println(string);     // Nov 03, 2014 - 07:13
     }
 }
