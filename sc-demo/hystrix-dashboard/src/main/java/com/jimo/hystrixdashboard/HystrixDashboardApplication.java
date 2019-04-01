8c4c448fb6ba326ab85b92936ae7087e67dc86f2
jimo <jimolonely@yahoo.com>
2019-04-01 09:01:32 +0800

    add hystrix dashboard

---
+++ sc-demo/hystrix-dashboard/src/main/java/com/jimo/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,19 @@
+package com.jimo.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author jimo
+ * @date 19-4-1 上午8:20
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+
+}
--- sc-demo/hystrix-feign/src/main/java/com/jimo/hystrixfeign/HystrixFeignApplication.java
+++ sc-demo/hystrix-feign/src/main/java/com/jimo/hystrixfeign/HystrixFeignApplication.java
@@ -2,7 +2,9 @@ package com.jimo.hystrixfeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -10,8 +12,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @date 19-3-29 下午5:14
  */
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableFeignClients
+@EnableHystrix
 public class HystrixFeignApplication {
 
     public static void main(String[] args) {
