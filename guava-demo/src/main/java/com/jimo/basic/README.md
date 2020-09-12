b9cf2dfe8f619e86bbac98911c11572c18ae19a4
jimolonely <jimolonely@yahoo.com>
2020-09-12 09:32:16 +0800

    add preconditions

--- guava-demo/src/main/java/com/jimo/basic/README.md
+++ guava-demo/src/main/java/com/jimo/basic/README.md
@@ -1,6 +1,8 @@
 
 # Optional
 
+避免直接使用null。
+
 ```java
     @Test
     public void testOptional() {
@@ -21,5 +23,22 @@
         // i3.get(); // java.lang.IllegalStateException: Optional.get() cannot be called on an absent value
         assertFalse(i3.isPresent());
     }
+```
+
+# Preconditions
+
+先决条件，该类包含了n个静态方法，检查变量是否满足条件；可以使代码更加整洁。
+
+```java
+@Test
+public void testPreconditions() {
+    int a = 10;
+    int b = 20;
+    List<Integer> list = new ArrayList<>();
+    assertThrows(IllegalArgumentException.class, () -> Preconditions.checkArgument(a > b, "%s must > %s", a, b));
+    assertThrows(NullPointerException.class, () -> Preconditions.checkNotNull(null));
+    assertThrows(IllegalStateException.class, () -> Preconditions.checkState(list.size() > 0));
+    assertThrows(IndexOutOfBoundsException.class, () -> Preconditions.checkElementIndex(10, list.size()));
+}
 ```
 
--- guava-demo/src/test/java/com/jimo/basic/BasicTest.java
+++ guava-demo/src/test/java/com/jimo/basic/BasicTest.java
@@ -1,9 +1,13 @@
 package com.jimo.basic;
 
 import com.google.common.base.Optional;
-import org.junit.Test;
+import com.google.common.base.Preconditions;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BasicTest {
 
@@ -25,5 +29,16 @@ public class BasicTest {
         final Optional<Integer> i3 = Optional.absent();
         // i3.get(); // java.lang.IllegalStateException: Optional.get() cannot be called on an absent value
         assertFalse(i3.isPresent());
+    }
+
+    @Test
+    public void testPreconditions() {
+        int a = 10;
+        int b = 20;
+        List<Integer> list = new ArrayList<>();
+        assertThrows(IllegalArgumentException.class, () -> Preconditions.checkArgument(a > b, "%s must > %s", a, b));
+        assertThrows(NullPointerException.class, () -> Preconditions.checkNotNull(null));
+        assertThrows(IllegalStateException.class, () -> Preconditions.checkState(list.size() > 0));
+        assertThrows(IndexOutOfBoundsException.class, () -> Preconditions.checkElementIndex(10, list.size()));
     }
 }
