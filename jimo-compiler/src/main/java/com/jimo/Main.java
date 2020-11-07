50a8fa384e708321a81d3cca5d00f27b7933b7c5
jimolonely <jimolonely@yahoo.com>
2020-11-07 13:28:33 +0800

    add jimo compiler project

---
+++ jimo-compiler/src/main/java/com/jimo/Main.java
@@ -0,0 +1,14 @@
+package com.jimo;
+
+/**
+ * main
+ *
+ * @author jimo
+ * @date 2020/11/7 13:27
+ * @since 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
